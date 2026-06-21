369adc3d395346ba1c099b8df41452872222f504
Muhammad-Saad-786 <sm8004393@gmail.com>
2026-06-22 04:18:44 +0500

    package.josn is updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 const BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8080/api";
-
+// Helper function to get headers with auth token if available
 const getHeaders = () => {
   const token = localStorage.getItem("token");
   return {
